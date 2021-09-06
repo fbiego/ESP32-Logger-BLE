aeacdd94af7d1d72c845c13d9cd730e8f53a8261
Felix Biego <fbiego.fb@gmail.com>
2021-09-06 09:03:24 +0300

    Update ESP32-Logger-BLE.ino

--- ESP32-Logger-BLE.ino
+++ ESP32-Logger-BLE.ino
@@ -3,6 +3,9 @@
 #define CHARACTERISTIC_UUID_RX    "fb1e4002-54ae-4a28-9f74-dfccb248601d"
 #define CHARACTERISTIC_UUID_TX    "fb1e4003-54ae-4a28-9f74-dfccb248601d"
 
+static BLECharacteristic* pCharacteristicTX;
+static BLECharacteristic* pCharacteristicRX;
+
 void setup(){
   
 }
